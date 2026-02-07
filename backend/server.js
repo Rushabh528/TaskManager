08911874e2dd7f4a7ce73e78b863b3dbdca2851f
Rushabh528 <rushabh2825@gmail.com>
2026-02-07 16:02:58 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const PORT = process.env.PORT || 5000;
 
 const taskRoutes = require("./routes/tasks");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(
+    cors({
+        origin: "*",
+    })
+); app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("DB connected"))
@@ -19,6 +23,6 @@ app.get("/", (req, res) => {
     res.send("API Running");
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log("Server running on port 5000");
 });
