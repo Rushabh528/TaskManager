a93c5ab98bb2929890c68f54f2cc9629d6f1c04b
Rushabh528 <rushabh2825@gmail.com>
2026-02-07 16:19:25 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.options("*", cors());
+// app.options("*", cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
